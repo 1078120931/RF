23d2e49b7be5d52deb798f94ee4849f9696e795b
1078120931 <139532911+1078120931@users.noreply.github.com>
2025-03-13 23:49:34 +0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -46,10 +46,10 @@ def main():
             explainer = shap.Explainer(best_model)
             shap_values = explainer.shap_values(df_subject)
             # 力图
-            shap.force_plot(explainer.expected_value[1], shap_values[1][0, :], df_subject.iloc[0, :], matplotlib=True)
+            shap.force_plot(explainer.expected_value, shap_values[0, :], df_subject.iloc[0, :], matplotlib=True)
             # 瀑布图
-            # ex = shap.Explanation(shap_values[1][0, :], explainer.expected_value[1], df_subject.iloc[0, :])
-            # shap.waterfall_plot(ex)
+            ex = shap.Explanation(shap_values[0, :], explainer.expected_value, df_subject.iloc[0, :])
+            shap.waterfall_plot(ex)
             st.pyplot(plt.gcf())
 
     st.set_page_config(page_title='IPN hemorrhage')
